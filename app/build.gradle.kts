94fed3b4e3688d26e3c1d4065f0c01260d3cd6f4
lekienvn7 <ltrgkien68.68@gmail.com>
2026-09-26 08:50:26 +0700

    26.9 - chore : Xóa các file không cần thiết

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,6 +34,8 @@ android {
 }
 
 dependencies {
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
     implementation(libs.activity.ktx)
     implementation(libs.appcompat)
     implementation(libs.constraintlayout)
@@ -41,4 +43,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.espresso.core)
     androidTestImplementation(libs.ext.junit)
+
 }
